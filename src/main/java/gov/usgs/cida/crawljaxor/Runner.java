33989ffef48a92977196dd940980a8d2bf71ba4b
Carl Schroedl <cschroedl@usgs.gov>
2015-08-10 16:53:17 -0500

    Creating CLI.

--- src/main/java/gov/usgs/cida/crawljaxor/Runner.java
+++ src/main/java/gov/usgs/cida/crawljaxor/Runner.java
@@ -1,5 +1,6 @@
 package gov.usgs.cida.crawljaxor;
 
+import com.beust.jcommander.JCommander;
 import com.crawljax.browser.EmbeddedBrowser;
 import java.io.File;
 import java.util.HashMap;
@@ -7,10 +8,13 @@ import java.util.Map;
 import com.crawljax.core.CrawljaxRunner;
 import com.crawljax.core.configuration.BrowserConfiguration;
 import com.crawljax.core.configuration.CrawljaxConfiguration;
+import com.crawljax.core.configuration.CrawljaxConfiguration.CrawljaxConfigurationBuilder;
 import com.crawljax.core.plugin.HostInterface;
 import com.crawljax.core.plugin.HostInterfaceImpl;
 import com.crawljax.core.state.StateVertex;
 import com.crawljax.core.state.StateVertexFactory;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
 
 /**
  * Use the plugin in combination with Crawljax.
@@ -25,28 +29,52 @@ public class Runner {
 	 * Entry point
 	 */
 
-	public static void main(String[] args) {
-		CrawljaxConfiguration.CrawljaxConfigurationBuilder builder = CrawljaxConfiguration.builderFor(URL);
+	public static void main(String[] args) throws IOException {
+		CliArgs cliArgs = new CliArgs();
+		new JCommander(cliArgs, args);
+		CrawljaxConfiguration config = getConfig(cliArgs);
+		CrawljaxRunner crawljax = new CrawljaxRunner(config);
+		crawljax.call();
+	}
+	
+	private static File createCleanDir(String dirName) throws IOException{
+		File dir = new File(dirName);
+		//clear existing dir
+		FileUtils.deleteDirectory(dir);
+		//re-create
+		dir.mkdirs();
+		return dir;
+	}
+	
+	private static CrawljaxConfiguration getConfig(CliArgs cliArgs) throws IOException{
+		String url = cliArgs.getUrl();
+		int maxDepth = cliArgs.getMaxDepth();
+		int maxNumberOfStates = cliArgs.getMaxNumberOfStates();
+		int numberOfBrowsers = cliArgs.getNumberOfBrowsers();
+		
+		CrawljaxConfiguration.CrawljaxConfigurationBuilder builder = CrawljaxConfiguration.builderFor(url);
 		builder.crawlRules().insertRandomDataInInputForms(false);
 
 		builder.crawlRules().click("a");
 
-		builder.setMaximumStates(MAX_NUMBER_STATES);
-		builder.setMaximumDepth(MAX_DEPTH);
-		builder.setBrowserConfig(new BrowserConfiguration(EmbeddedBrowser.BrowserType.PHANTOMJS, NUM_BROWSERS));
+		builder.setMaximumStates(maxNumberOfStates);
+		builder.setMaximumDepth(maxDepth);
+		builder.setBrowserConfig(new BrowserConfiguration(EmbeddedBrowser.BrowserType.PHANTOMJS, numberOfBrowsers));
+
+		//override default factory so that states are only compared based on url
 		builder.setStateVertexFactory(new StateVertexFactory() {
 			@Override
 			public StateVertex newStateVertex(int id, String url, String name, String dom, String strippedDom) {
 				return new StateVertexUrlEqualityImpl(id, url, name, dom, strippedDom);
 			}
 		});
+		
+		String outputDir = cliArgs.getOutputDir();
+		File dir = createCleanDir(outputDir);
 		Map<String, String> parameters = new HashMap<>();
-		File dir = new File("out");
-		dir.mkdir();
 		HostInterface hostInterface = new HostInterfaceImpl(dir, parameters);
 		builder.addPlugin(new SaveToFilePlugin(hostInterface));
-
-		CrawljaxRunner crawljax = new CrawljaxRunner(builder.build());
-		crawljax.call();
+		
+		return builder.build();
 	}
 }
