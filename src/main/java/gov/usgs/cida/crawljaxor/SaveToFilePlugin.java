7202630c2d358c636b821d8fd645df1b99186277
Carl Schroedl <cschroedl@usgs.gov>
2015-08-11 15:45:56 -0500

    Switching to collision-resistant hash function.

--- src/main/java/gov/usgs/cida/crawljaxor/SaveToFilePlugin.java
+++ src/main/java/gov/usgs/cida/crawljaxor/SaveToFilePlugin.java
@@ -7,6 +7,7 @@ import com.crawljax.core.plugin.HostInterface;
 import com.crawljax.core.plugin.OnNewStatePlugin;
 import com.crawljax.core.state.StateVertex;
 import java.io.IOException;
+import gov.usgs.cida.simplehash.SimpleHash;
 
 public class SaveToFilePlugin implements OnNewStatePlugin {
 
@@ -22,9 +23,8 @@ public class SaveToFilePlugin implements OnNewStatePlugin {
 		String dom = context.getBrowser().getStrippedDom();
 		String url = context.getCurrentState().getUrl();
 
-			//Native Java String Hash Code is stable between versions
-		//as of v1.2 according to: http://stackoverflow.com/a/785150
-		String fileName = url.hashCode() + ".html";
+		String hashedUrl = SimpleHash.hash(url, "SHA-1");
+		String fileName = hashedUrl + ".html";
 		File parentDirs = hostInterface.getOutputDirectory();
 		File file = new File(parentDirs, fileName);
 		file.getParentFile().mkdirs();
